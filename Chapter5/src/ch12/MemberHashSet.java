24a47549bb7fac32e8bda797f2d840e4cfddc6fe
taeeonkim <myhom43@gmail.com>
2023-11-25 13:31:25 +0900

    array, hashset, treeset, hashmap

---
+++ Chapter5/src/ch12/MemberHashSet.java
@@ -0,0 +1,42 @@
+package ch12;
+
+import java.util.HashSet;
+
+public class MemberHashSet {
+	
+	private HashSet<Member> hashSet;
+	
+	public MemberHashSet() {
+		hashSet = new HashSet<>();
+	}
+	
+	
+	public void addMember(Member member) {
+		hashSet.add(member);
+	}
+	
+	public boolean removeMember(int memberId) {
+		
+//		for(int i=0; i<hashSet.size(); i++) {
+//			Member member = hashSet.get(i);
+//			
+//			int tempId = member.getMemberId();
+//			if(tempId == memberId) {
+//				hashSet.remove(i);
+//				return true;
+//			}
+//		}
+		
+		System.out.println(memberId +"가 존재하지 않습니다.");
+		return false;
+	}
+	
+	public void showAllMember() {
+		for(Member member : hashSet) {
+			System.out.println(member);
+		}
+		System.out.println();
+	}
+}
+
+
