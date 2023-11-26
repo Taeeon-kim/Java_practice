d408673db211e2f2b7a632dad9ed5d521bdf3ede
taeeonkim <myhom43@gmail.com>
2023-11-26 09:33:18 +0900

    예외처리

---
+++ Chapter6/src/ch08/ArrayIndexException.java
@@ -0,0 +1,23 @@
+package ch08;
+
+public class ArrayIndexException {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] arr = {1,2,3,4,5};
+		
+		try {
+			for(int i =0; i<=5; i++ ) {
+				System.out.println(arr[i]);
+			}
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.toString());
+		}
+		
+		System.out.println("here!!!");
+		
+	}
+
+}
---
+++ Chapter6/src/ch08/AutoCloseableObj.java
@@ -0,0 +1,11 @@
+package ch08;
+
+public class AutoCloseableObj implements AutoCloseable {
+
+	@Override
+	public void close() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("closing....");
+	}
+
+}
