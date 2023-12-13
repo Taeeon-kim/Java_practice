b32e7855f85d36b073092f428f963cb94aa3c9c1
taeeonkim <myhom43@gmail.com>
2023-12-13 23:05:01 +0900

    쓰레드, 데코레이 패턴

---
+++ Chapter6/src/ch19/EtiopiaAmericano.java
@@ -0,0 +1,11 @@
+package ch19;
+
+public class EtiopiaAmericano extends Coffee {
+
+	@Override
+	public void brewing() {
+		// TODO Auto-generated method stub
+		System.out.println("Etiopia Americano");
+	}
+	
+}
