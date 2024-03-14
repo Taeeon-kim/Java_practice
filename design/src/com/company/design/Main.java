1699dc0dd6856d19d0a44b5db5422a135913c19e
taeeonkim <myhom43@gmail.com>
2024-03-14 22:57:52 +0900

    프록시패턴

--- design/src/com/company/design/Main.java
+++ design/src/com/company/design/Main.java
@@ -1,6 +1,12 @@
 package com.company.design;
 
 import com.company.design.adapter.*;
+import com.company.design.aop.AopBrowser;
+import com.company.design.proxy.Browser;
+import com.company.design.proxy.BrowserProxy;
+import com.company.design.proxy.IBrowser;
+
+import java.util.concurrent.atomic.AtomicLong;
 //import com.company.design.singleton.AClazz;
 //import com.company.design.singleton.BClazz;
 //import com.company.design.singleton.SocketClient;
@@ -16,19 +22,50 @@ public class Main {
 //        System.out.println("두개의 객체가 동일한가?");
 //        System.out.println(aClient.equals(bClient));
 
-        HairDryer hairDryer = new HairDryer();
-        connect(hairDryer);
+//        HairDryer hairDryer = new HairDryer();
+//        connect(hairDryer);
+//
+//        Cleaner cleaner = new Cleaner();
+//
+//        Electronic110V adapter = new SocketAdapter(cleaner);
+//        connect(adapter);
+//
+//        AirConditioner airConditioner = new AirConditioner();
+//
+//        Electronic110V airAdapter = new SocketAdapter(airConditioner);
+//        connect(airAdapter);
 
-        Cleaner cleaner = new Cleaner();
 
-        Electronic110V adapter = new SocketAdapter(cleaner);
-        connect(adapter);
+//        Browser browser = new Browser("www.naver.com");
+//        browser.show();
+//        browser.show();
+//        browser.show();
+//        browser.show();
 
-        AirConditioner airConditioner = new AirConditioner();
+//        IBrowser browser = new BrowserProxy("www.naver.com");
+//        browser.show();
+//        browser.show();
+//        browser.show();
+//        browser.show();
+//        browser.show();
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
 
-        Electronic110V airAdapter = new SocketAdapter(airConditioner);
-        connect(airAdapter);
+        IBrowser aopBrowser = new AopBrowser("www.naver.com", ()->{
+    System.out.println("before");
+    start.set(System.currentTimeMillis());
+        },()->{
+            long now = System.currentTimeMillis();
+            end.set(now-start.get());
+        });
+    aopBrowser.show();
+        System.out.println("loading time: "+end.get());
+
+        aopBrowser.show();
+        System.out.println("loading time: "+end.get());
     }
+
+
 
     // 콘센트
     public static void connect(Electronic110V electronic110V) {
