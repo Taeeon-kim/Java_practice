4c7546e8c8a6b6095e8d9f6e8a154564ead25794
taeeonkim <myhom43@gmail.com>
2024-03-14 23:19:52 +0900

    데코레이터패턴

--- design/src/com/company/design/Main.java
+++ design/src/com/company/design/Main.java
@@ -1,68 +1,29 @@
 package com.company.design;
 
 import com.company.design.adapter.*;
-import com.company.design.aop.AopBrowser;
-import com.company.design.proxy.Browser;
-import com.company.design.proxy.BrowserProxy;
-import com.company.design.proxy.IBrowser;
 
-import java.util.concurrent.atomic.AtomicLong;
+import com.company.design.decorator.*;
+
 //import com.company.design.singleton.AClazz;
 //import com.company.design.singleton.BClazz;
 //import com.company.design.singleton.SocketClient;
 
 public class Main {
     public static void main(String[] args) {
-//        AClazz aClazz = new AClazz();
-//        BClazz bClazz = new BClazz();
-//
-//        SocketClient aClient = aClazz.getSocketClient();
-//        SocketClient bClient = bClazz.getSocketClient();
-//
-//        System.out.println("두개의 객체가 동일한가?");
-//        System.out.println(aClient.equals(bClient));
 
-//        HairDryer hairDryer = new HairDryer();
-//        connect(hairDryer);
-//
-//        Cleaner cleaner = new Cleaner();
-//
-//        Electronic110V adapter = new SocketAdapter(cleaner);
-//        connect(adapter);
-//
-//        AirConditioner airConditioner = new AirConditioner();
-//
-//        Electronic110V airAdapter = new SocketAdapter(airConditioner);
-//        connect(airAdapter);
+        ICar audi = new Audi(1000);
+        audi.showPrice();
+        //a3
+    ICar audi3 = new A3(audi, "A3");
+    audi3.showPrice();
 
+        //a4
+    ICar audi4 = new A4(audi, "A4");
+    audi4.showPrice();
+        //a5
 
-//        Browser browser = new Browser("www.naver.com");
-//        browser.show();
-//        browser.show();
-//        browser.show();
-//        browser.show();
-
-//        IBrowser browser = new BrowserProxy("www.naver.com");
-//        browser.show();
-//        browser.show();
-//        browser.show();
-//        browser.show();
-//        browser.show();
-        AtomicLong start = new AtomicLong();
-        AtomicLong end = new AtomicLong();
-
-        IBrowser aopBrowser = new AopBrowser("www.naver.com", ()->{
-    System.out.println("before");
-    start.set(System.currentTimeMillis());
-        },()->{
-            long now = System.currentTimeMillis();
-            end.set(now-start.get());
-        });
-    aopBrowser.show();
-        System.out.println("loading time: "+end.get());
-
-        aopBrowser.show();
-        System.out.println("loading time: "+end.get());
+        ICar audi5 = new A5(audi,"A5");
+        audi5.showPrice();
     }
 
 
