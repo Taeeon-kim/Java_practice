72ee213ed81ff5f3102629a7680eb08038e21c33
taeeonkim <myhom43@gmail.com>
2024-03-31 23:49:26 +0900

    spring  MVC, template, api, repository, test, domain

---
+++ hell-springs/src/main/java/hello/hellsprings/HellSpringsApplication.java
@@ -0,0 +1,13 @@
+package hello.hellsprings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellSpringsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellSpringsApplication.class, args);
+    }
+
+}
---
+++ hell-springs/src/main/java/hello/hellsprings/controller/HelloController.java
@@ -0,0 +1,48 @@
+package hello.hellsprings.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello") // NOTE: 해당 path 경로 이름으로 매핑해줌
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!");
+        return "hello"; // NOTE: 해당 이름의 html 이름으로 렌더링 시켜줌
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
