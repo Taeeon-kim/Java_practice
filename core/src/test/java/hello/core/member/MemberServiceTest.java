1afb941e93912f469ca4d862901827e4797bf30b
taeeonkim <myhom43@gmail.com>
2024-06-27 22:42:13 +0900

    test 역시 수정

--- core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,14 +1,22 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
+    MemberService memberService;
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join(){
         //given
-        MemberService memberService = new MemberServiceImpl();
+
         Member member = new Member(1L, "memberA",Grade.VIP);
 
         //when
