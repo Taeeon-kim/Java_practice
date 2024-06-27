7895c8a6aea49be2681044167d1d3f481e04c91a
taeeonkim <myhom43@gmail.com>
2024-06-28 00:07:52 +0900

    역할과 구현 분리 리팩토링

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,13 +11,19 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
-    public MemberService memberService(){ // 맴버서비스
-        return new MemberServiceImpl(new MemoryMemberRepository()); // 생성자 호출, 참조값을 생성자에 주입해줌
+    public MemberService memberService(){ // 맴버서비스 역할
+        return new MemberServiceImpl(memberRepository()); // 생성자 호출, 참조값을 생성자에 주입해줌
+    }
+    public OrderService orderService(){ // 오더 서비스 역할
+        return new OrderServiceImpl(memberRepository(), discountPolicy()); // 생성자 호출, 참조값을 생성자에 주입해줌
     }
 
-    public OrderService orderService(){ // 오더 서비스
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy()); // 생성자 호출, 참조값을 생성자에 주입해줌
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository(); // 구현체
     }
 
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy(); // 구현체
+    }
 
 }
