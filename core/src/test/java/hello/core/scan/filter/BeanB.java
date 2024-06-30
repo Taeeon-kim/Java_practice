d72636d31a39b8c4cb7c3ae02e29729dba478ab2
taeeonkim <myhom43@gmail.com>
2024-07-01 01:45:49 +0900

    중복 빈 오류 테스트

---
+++ core/src/test/java/hello/core/scan/filter/BeanB.java
@@ -0,0 +1,6 @@
+package hello.core.scan.filter;
+
+@MyExcludeComponent // excludecomponent 애노테이션을 달았기때문에 빈에 등록되지않음
+public class BeanB {
+
+}
---
+++ core/src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -0,0 +1,34 @@
+package hello.core.scan.filter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.context.annotation.ComponentScan.*;
+
+public class ComponentFilterAppConfigTest {
+
+    @Test
+    void filterTest(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
+        BeanA beanA = ac.getBean("beanA", BeanA.class);
+        Assertions.assertThat(beanA).isNotNull();
+
+//        BeanB beanB = ac.getBean("beanB", BeanB.class); // component에서 제외되었기때문에 오류발생
+        assertThrows(NoSuchBeanDefinitionException.class, ()-> ac.getBean("beanB", BeanB.class)); // NoSuchBeanDefinitionException 라는 예외 클래스 가 발생 하는지 체크
+    }
+@Configuration
+@ComponentScan(
+        includeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyIncludeComponent.class),
+        excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyExcludeComponent.class)
+)
+    static class ComponentFilterAppConfig{
+
+}
+}
