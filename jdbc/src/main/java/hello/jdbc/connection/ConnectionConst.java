03e25cdd9a2b410d81dee9e6b5fa8cf927b24842
taeeonkim <myhom43@gmail.com>
2024-10-05 00:06:43 +0900

    JDBC DriverManager, connection, PreparedStatement, Member data , try,catch

---
+++ jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,9 @@
+package hello.jdbc.connection;
+
+
+public abstract class ConnectionConst { // 생성못하게 추상화 abstract
+    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+
+}
---
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -0,0 +1,87 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.connection.DBConnectionUtil;
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.*;
+import java.util.NoSuchElementException;
+
+/**
+* JDBC - DriverManager 사용
+*/
+
+@Slf4j
+public class MemberRepositoryV0 {
+    public Member save(Member member) throws SQLException {
+        String sql = "insert into member(member_id, money) values(?, ?)";
+        Connection con = null;
+
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, member.getMemberId());
+            pstmt.setInt(2, member.getMoney());
+            pstmt.executeUpdate();
+            return member;
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null); // con, pstmt에서도 에러가 발생할수있어서 이렇게 처리
+        }
+
+    }
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try{
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            rs = pstmt.executeQuery();
+            if(rs.next()){
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else{
+                throw new NoSuchElementException("member not found memberId = "+memberId);
+            }
+
+        }catch (SQLException e){
+            log.error("error",e);
+            throw e;
+        } finally{
+            close(con, pstmt, rs);
+        }
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs){
+        if(stmt != null){
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                log.error("error", e);
+            }
+        }
+        if(con != null){
+            try {
+                con.close();  // tcp/ip 를 끊어줘야함, 이 코트드 무조건 실행되도록 보장해줘야함 필수!
+            } catch (SQLException e) {
+                log.error("error", e);
+            }
+        }
+    }
+
+    private static Connection getConnection() {
+        return DBConnectionUtil.getConnection();
+    }
+}
