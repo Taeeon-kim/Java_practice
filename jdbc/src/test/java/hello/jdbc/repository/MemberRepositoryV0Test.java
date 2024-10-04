e271b7718ae4a9b809525c72c7ec4762ab21659d
taeeonkim <myhom43@gmail.com>
2024-10-05 02:22:39 +0900

    update, delete, @AfterEach 중복 데이터 없애기위한 delete로직 추가

--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -3,9 +3,12 @@ package hello.jdbc.repository;
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,11 +17,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
+    Member member = new Member("memberV5", 10000);
+    @AfterEach
+    void afterEach() throws SQLException { // 테스트를 위해 중복되는 등록을 없애기위해 테스트완료후 삭제는 여기서 진행
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(()-> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+    }
 
     @Test
     void crud() throws SQLException {
+
+
         // save
-        Member member = new Member("memberV2", 10000);
+
         repository.save(member);
 
         //findById
@@ -27,5 +40,16 @@ class MemberRepositoryV0Test {
         log.info("member == findMember {}", member == findMember);
         log.info("member equals findMember {}", member.equals(findMember));
         assertThat(findMember).isEqualTo(member);
+
+
+        //update: money 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+//        //delete
+//        repository.delete(member.getMemberId());
+//        assertThatThrownBy(()-> repository.findById(member.getMemberId()))
+//                .isInstanceOf(NoSuchElementException.class);
     }
 }
