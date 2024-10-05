1fb472246af7eadc1d275fef5402cac3abf02ca5
taeeonkim <myhom43@gmail.com>
2024-10-05 16:40:07 +0900

    DataSource 사용, Hikari Connectionool 사용, driverManager사용 테스트케이스 추가

--- jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -28,9 +29,24 @@ public class ConnectionTest {
         userDataSource(dataSource);
     }
 
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        //커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10); // default 10 이지만 보여주기위해
+        dataSource.setPoolName("Mypool");
+
+        userDataSource(dataSource);
+        Thread.sleep(1000);
+    }
+
     private void userDataSource(DataSource dataSource) throws SQLException {
         Connection con1 = dataSource.getConnection(); // URL, USERNAME, PASSORD를 이제 쓸필요없음
         Connection con2 = dataSource.getConnection();
+
         log.info("connection={}, class={}", con1, con1.getClass());
         log.info("connection={}, class={}", con2, con2.getClass());
     }
