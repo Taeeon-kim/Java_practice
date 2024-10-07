cb99fde4e3c6b29da0f6432b7fa82235ea814842
taeeonkim <myhom43@gmail.com>
2024-10-07 16:42:42 +0900

    트랜잭션 AOP 적용 @Transactional

--- jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_1.java
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_1.java
@@ -55,16 +55,4 @@ public class MemberServiceV3_1 {
             throw new IllegalStateException("이체중 예외 발생");
         }
     }
-
-    private void release(Connection con) {
-        if(con != null){
-            try{
-                con.setAutoCommit(true); // 커넥션 풀을 고려해서 default를 위해 autoCommit을 true
-                con.close();
-            } catch(Exception e){
-                log.info("error", e);
-            }
-        }
-
-    }
 }
--- jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_3.java
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -2,43 +2,30 @@ package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV3;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
-import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
 
 /**
- * 트랜잭션 - 트랜잭션 템플릿
+ * 트랜잭션 - @Transactional AOP
 */
 
 @Slf4j
-public class MemberServiceV3_2 {
+public class MemberServiceV3_3 {
 
 //    private final PlatformTransactionManager transactionManager;
-    private final TransactionTemplate txTemplate;
     private final MemberRepositoryV3 memberRepository;
 
-    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
-        this.txTemplate = new TransactionTemplate(transactionManager);
+    public MemberServiceV3_3( MemberRepositoryV3 memberRepository) {
         this.memberRepository = memberRepository;
     }
 
+    @Transactional // Transaction 시작, 트랜잭션 AOP 적용, 프록시(for 트랜잭션 탬플릿, 트랜잭션 매니저등) 역할해줌
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-
-        txTemplate.executeWithoutResult((status)-> {
-            try {
                 bizLogic(fromId, toId, money);
-            } catch (SQLException e) {
-                throw new IllegalStateException(e);
-            }
-        });
-
     }
 
     private void bizLogic(String fromId, String toId, int money) throws SQLException {
