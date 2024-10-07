ad8cfc493ee452bd61fdaea169de0d3ffd4f9dfa
taeeonkim <myhom43@gmail.com>
2024-10-07 23:27:00 +0900

    properties로 빼서 사용 @SpringBootTest, @TestConfiguration, @Bean

--- jdbc/src/test/java/hello/jdbc/service/MemberServiceV3_4Test.java
+++ jdbc/src/test/java/hello/jdbc/service/MemberServiceV3_4Test.java
@@ -5,7 +5,6 @@ import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
@@ -25,12 +24,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
-* 트랜잭션 - 트랜잭션 AOP
+* 트랜잭션 - DataSource, TransactionManager 자동 등록
 * */
 
 @Slf4j
 @SpringBootTest
-class MemberServiceV3_3Test {
+class MemberServiceV3_4Test {
     public static final String MEMBER_A = "memberA";
     public static final String MEMBER_B = "memberB";
     public static final String MEMBER_EX = "ex";
@@ -42,19 +41,16 @@ class MemberServiceV3_3Test {
 
     @TestConfiguration
     static class TestConfig{
-        @Bean
-        DataSource dataSource(){
-            return new DriverManagerDataSource(URL, USERNAME, PASSWORD);
-        }
 
-        @Bean
-        PlatformTransactionManager transactionManager(){
-            return new DataSourceTransactionManager(dataSource());
+        private final DataSource dataSource;
+
+        public TestConfig(DataSource dataSource) {
+            this.dataSource = dataSource;
         }
 
         @Bean
         MemberRepositoryV3 memberRepositoryV3(){
-            return new MemberRepositoryV3(dataSource());
+            return new MemberRepositoryV3(dataSource);
         }
 
         @Bean
