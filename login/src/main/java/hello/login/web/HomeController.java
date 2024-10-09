7193d7fb6e5491b8062a4957ec65da97f18a0865
taeeonkim <myhom43@gmail.com>
2024-10-10 02:35:25 +0900

    request의 HttpSession 사용

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -18,25 +20,64 @@ import javax.servlet.http.HttpServletResponse;
 public class HomeController {
 
    private final MemberRepository memberRepository;
-
+   private final SessionManager sessionManager;
 //    @GetMapping("/")
     public String home() {
-        System.out.println("here");
+
         return "home";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLogin(@CookieValue(name="memberId", required = false) Long memberId, Model model){
         if(memberId == null){
             return "home";
         }
-        System.out.println("there");
+
         System.out.println("memberId = " + memberId);
         System.out.println("memberRepository.findAll() = " + memberRepository.findAll());
         //로그인
         Member loginMember = memberRepository.findById(memberId);
 
-        System.out.println("loginMember = " + loginMember);
+        //세션에 회원 데이터가 없으면 Home
+        if(loginMember == null){
+            return "home";
+        }
+
+        //세션이 유지되면 로그인홈으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+
+    }
+
+//    @GetMapping("/")
+    public String homeLoginV2(HttpServletRequest request, Model model){
+
+        //세션 관리자에 저장된 회원 정보 조회
+        Member member = (Member)sessionManager.getSession(request);
+
+
+        if(member == null){
+            return "home";
+        }
+        //로그인 성공시 해당 member정보를  model로 전달
+        model.addAttribute("member", member);
+        return "loginHome";
+
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3(HttpServletRequest request, Model model){
+
+        HttpSession session = request.getSession(false);
+        System.out.println("session = " + session);
+        if(session == null){
+            return "home";
+        }
+
+        Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+        //세션 관리자에 저장된 회원 정보 조회
+
+        //로그인
         if(loginMember == null){
             return "home";
         }
