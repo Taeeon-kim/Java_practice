145d3bddba6e05b49c8174aacd818ab800c1b1ff
taeeonkim <myhom43@gmail.com>
2024-10-10 02:45:30 +0900

    spring @SessionAttribute 사용 및 default parameter JSESSIONID 제거를 위한 properties 값 추가

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -65,7 +66,7 @@ public class HomeController {
 
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model){
 
         HttpSession session = request.getSession(false);
@@ -76,6 +77,19 @@ public class HomeController {
 
         Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
         //세션 관리자에 저장된 회원 정보 조회
+
+        //로그인
+        if(loginMember == null){
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3Spring(@SessionAttribute(name= SessionConst.LOGIN_MEMBER, required = false) Member loginMember , Model model){
 
         //로그인
         if(loginMember == null){
